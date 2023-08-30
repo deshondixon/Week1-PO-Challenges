9d65c401516fb2f81213f6059626c4789c204728
Seb Shader <sebfumaster@aol.com>
2023-08-29 17:50:26 -0700

    use const in js

--- OddishOrEvenish.js
+++ OddishOrEvenish.js
@@ -3,7 +3,7 @@
 export function oddishOrEvenish(inNum) {
     inNum = inNum.toString().replace(/[^0-9]/g, '').split('');
 
-    let num = inNum.reduce((previous, current) => previous + +current, 0);
+    const num = inNum.reduce((previous, current) => previous + +current, 0);
     //console.log(num);
     return num & 1 ? "Oddish" : "Evenish";
 }
