17b732ae8ab6db1f133e0fe68782959eb2561e25
DeShon Dixon <deshondixon@gmail.com>
2023-08-30 08:13:38 -0700

    acp

--- OddishOrEvenish.js
+++ OddishOrEvenish.js
@@ -1,8 +1,7 @@
-
-export function oddishOrEvenish(inNum) {
-    inNum = inNum.toString().replace(/[^0-9]/g, '').split('');
-
-    const num = inNum.reduce((previous, current) => previous + +current, 0);
-    //console.log(num);
-    return num & 1 ? "Oddish" : "Evenish";
+function oddishOrEvenish(num) {
+    const digitSum = num.toString().split('').reduce((sum, digit) => sum + parseInt(digit), 0);
+    return digitSum % 2 === 0 ? "Evenish" : "Oddish";
 }
+
+console.log(oddishOrEvenish(60));
+console.log(oddishOrEvenish(43));
